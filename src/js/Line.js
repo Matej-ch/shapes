d99851cced90df4c607d458eda50dd3184a3775c
Matej <matejchalachan@gmail.com>
2021-10-02 19:03:31 +0200

    closes #6

--- src/js/Line.js
+++ src/js/Line.js
@@ -1,7 +1,29 @@
+import {randomColor, randomPosition} from "./helper";
+
 class Line {
-    constructor() {
+    constructor({x = 0,y= 0,dirX = 10,dirY = 10,fillColor = randomColor} ={}) {
 
     }
+
+    draw(ctx,fillShape) {
+        ctx.beginPath();
+        ctx.fillStyle = ctx.strokeStyle = this.fillColor;
+
+        ctx.moveTo(this.x,this.y);
+        ctx.lineTo(this.dirX,this.dirY);
+
+        ctx.stroke();
+
+    }
+
+    init(canvas) {
+        this.x = randomPosition(0,canvas.width);
+        this.y = randomPosition(0,canvas.height);
+        this.dirX = randomPosition(0,canvas.width);
+        this.dirY =  randomPosition(0,canvas.height);
+        this.fillColor = randomColor();
+    }
+
 }
 
 export default Line;
